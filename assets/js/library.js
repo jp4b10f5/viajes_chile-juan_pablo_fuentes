5324d5b503b055e256f9433ceba038c72da3b746
Juan Pablo Fuentes <jfuentes03@alumnos.estatlcft.cl>
2024-03-27 21:00:33 -0300

    primer commit

---
+++ assets/js/library.js
@@ -0,0 +1,7 @@
+function grabaForm() {
+
+    var nombreApellido = document.getElementById('exampleInputEmail1').value;
+    var correoElectronico = document.getElementById('exampleInputPassword1').value;
+    alert("Gracias por contactar a Viajes Chile. Uno de nuestros agentes pronto se pondra en contacto con usted ");
+}
+
